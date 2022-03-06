2428684f0a3bc7524f551d63c20af7f76143487e
Swetha M <mswetha1708@gmail.com>
2022-03-06 09:41:05 +0530

    First commit

---
+++ app/src/main/java/com/example/project_swetha/LauncherActivity.java
@@ -0,0 +1,102 @@
+package com.example.project_swetha;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private int TIME_OUT = 500;
+    private static final int PERMISSION_ID = 44;
+    private String[] permissionString = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO };
+
+    TextView permission_denied_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        setContentView(R.layout.activity_launcher);
+
+        permission_denied_text = (TextView) findViewById(R.id.permission_text);
+
+        if (!checkPermissions()){
+            Log.d("Permission","Insufficient");
+            requestPermissions();
+        }
+        else{
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LauncherActivity.this, Recording.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 200);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+
+        boolean all_permission_granted = true;
+        if (grantResults.length == permissionString.length){
+            for (int i=0;i < grantResults.length; ++i){
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    all_permission_granted = false;
+                    break;
+                }
+            }
+
+            if (all_permission_granted){
+                //Start recording activity
+                Intent intent = new Intent(LauncherActivity.this, Recording.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+        if (!all_permission_granted)
+            permission_denied_text.setText("Please provide the necessary permissions to start using the app");
+        return;
+    }
+
+
+    // Utility functions for checking permissions
+
+    private boolean checkPermissions(){
+
+        if( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ){
+            return true;
+        }
+        return false;
+    }
+
+
+    private void requestPermissions(){
+        ActivityCompat.requestPermissions(
+                this,
+                permissionString,
+                PERMISSION_ID
+        );
+    }
+}
---
+++ app/src/main/java/com/example/project_swetha/MainActivity.java
@@ -0,0 +1,301 @@
+package com.example.project_swetha;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.location.Location;
+import android.location.LocationManager;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    // initializing
+    // FusedLocationProviderClient
+    // object
+
+
+
+
+
+
+
+
+    /************ CODE OPENS HERE********************
+    FusedLocationProviderClient mFusedLocationClient;
+
+    // Initializing other items
+    // from layout file
+    TextView latitudeTextView, longitTextView;
+    int PERMISSION_ID = 44;
+    private static final int pic_id = 123;
+
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        latitudeTextView = findViewById(R.id.latTextView);
+        longitTextView = findViewById(R.id.lonTextView);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+        // method to get the location
+
+
+        // By ID we can get each component
+        // which id is assigned in XML file
+        // get Buttons and imageview.
+        camera_open_id = (Button)findViewById(R.id.camera_button);
+        click_image_id = (ImageView)findViewById(R.id.click_image);
+
+        // Camera_open button is for open the camera
+        // and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                getLastLocation();
+                Intent camera_intent
+                        = new Intent(MediaStore
+                        .ACTION_IMAGE_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(camera_intent, pic_id);
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitudeTextView.setText(location.getLatitude() + "");
+                            longitTextView.setText(location.getLongitude() + "");
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude() + "");
+            longitTextView.setText("Longitude: " + mLastLocation.getLongitude() + "");
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+
+            // BitMap is data structure of image file
+            // which stor the image in memory
+            Bitmap photo = (Bitmap)data.getExtras()
+                    .get("data");
+//            android.graphics.Matrix matrix = new Matrix();
+//            float scaleWidth = ((float) 500) / 640;
+//            float scaleHeight = ((float) 500) / 1386;
+//            matrix.postScale(scaleWidth, scaleHeight);
+//            matrix.postRotate(45);
+//
+//            photo = Bitmap.createBitmap(photo, 0, 0, 500, 500, matrix, true);
+            // Set the image in imageview for display
+            Canvas canvas = new Canvas(photo);
+            canvas.drawBitmap(photo, 0, 0, null);
+            float scale = getResources().getDisplayMetrics().density;
+
+            Paint p = new Paint();
+            // Find the colour , position, size of the text and the value to be written.
+            p.setTypeface(Typeface.create("Arial",Typeface.BOLD));
+            p.setTextSize(10);
+            Paint.FontMetrics fm = new Paint.FontMetrics();
+            p.setColor(Color.BLACK);
+            p.getFontMetrics(fm);
+            int margin = 5;
+            canvas.drawRect(5 - margin, 15 + fm.top - margin,
+                    5 + p.measureText(latitudeTextView.getText().toString() + ","+ longitTextView.getText().toString()) + margin, 15 + fm.bottom
+                            + margin, p);
+
+            p.setColor(Color.WHITE);
+            canvas.drawText(latitudeTextView.getText().toString() + ","+ longitTextView.getText().toString() , 5,15,p);
+            click_image_id.setImageBitmap(photo);
+            String path = this.getExternalFilesDir(null).getAbsolutePath() + "/Download";// this is the sd card
+                                                                                                                                                                                                                                                                                                                                                                                            Log.i("Path:", path);
+                                                                                                                                                                                                                                                                                                                                                                                            File folder = new File(path);
+                                                                                                                                                                                                                                                                                                                                                                                            if (!folder.exists()) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          File wallpaperDirectory = new File(path);
+                                                                                                                                                                                                                                                                                                                                                                                                wallpaperDirectory.mkdirs();
+                                                                                                                                                                                                                                                                                                                                                                                            }
+            OutputStream fOut = null;
+            File file = new File(path, "MyImage.jpg");
+            try {
+                fOut = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            photo.compress(Bitmap.CompressFormat.JPEG, 20, fOut);
+            try {
+                fOut.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fOut.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    ***************/
+
+}
